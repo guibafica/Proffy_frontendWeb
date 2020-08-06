072427e56404a5f4abf7a8bb031d2cc7eb86d4ec
Guilherme Bafica <gb.engcomp@hotmail.com>
2020-08-06 18:50:05 -0300

    CHORE: Aplicaçao finalizada

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -2,34 +2,56 @@ import React from 'react';
 
 import whatsapp from '../../assets/images/icons/whatsapp.svg';
 
+import api from '../../services/api';
+
 import './style.css';
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id,
+    })
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://avatars1.githubusercontent.com/u/57121069?s=460&u=a2338031d1addfadda298d9b3c47684525eb0c42&v=4" alt="Guilherme Bafica"/>
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Guilherme Bafica</strong>
-          <span>Programação</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Entusiasta das melhores tecnologias de química avançada.
-        <br /><br />
-        Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
-      </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 80,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
-          <img src={whatsapp} alt="whatsapp"/>
+        <a 
+          target="_blank" 
+          onClick={createNewConnection} 
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
+          <img src={whatsapp} alt="whatsapp" />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
